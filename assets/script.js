d7e0a69689f5466a74f0cda03f370beb1cffef7d
Javier Dominguez II <Javi7057@gmail.com>
2021-06-17 20:16:26 -0500

    search-form complete

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+var searchContainerEl = document.getElementById("search-form-container");
+var searchEl = document.getElementById("search");
+
+var getSearch = function(event) {
+    // prevent page refresh
+    event.preventDefault();
+
+    // get value from input element
+    var searchTopic = searchEl.value.trim();
+
+    // console log may be deleted later
+    console.log(searchTopic);
+};
+
+searchContainerEl.addEventListener("submit", getSearch);

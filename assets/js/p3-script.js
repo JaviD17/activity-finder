ada54857bdc05cd8eccd44318cdbdfd85ab27c9d
E Lazar <emiliosalazar4@gmail.com>
2021-06-22 18:31:31 -0500

    updated variables

--- assets/js/p3-script.js
+++ assets/js/p3-script.js
@@ -1,22 +1,21 @@
 var getSearchTerm = function() {
     var queryString = document.location.href
-    var brewID = queryString.split("#")[1]
+    var brewName = queryString.split("#")[1]
     console.log(brewID)
-generateBreweries(brewID)
+generateBreweries(brewName)
 }
 
 var generateBreweries = function() {
-    var apiUrl = "https://api.openbrewerydb.org/breweries/" + brewID;
+    var apiUrl = "https://api.openbrewerydb.org/breweries?by_name" + brewName;
     fetch(apiUrl)
     .then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                var brewName = document.querySelector(".brew-name");
-                brewName.textContent = data.brew;
+                
+                brewCall.textContent = brewName
                 var brewAddress = document.querySelector(".brew-address");
                 brewAddress.textContent = data.street + " " + data.city + ", " + data.state;
                 }
             )}
     }
     )}
-getSearchTerm();

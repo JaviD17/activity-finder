926b53dbf26ef7e0c5018a4bbb6d7550a3e0c43a
E Lazar <emiliosalazar4@gmail.com>
2021-06-22 20:28:47 -0500

    updated

--- assets/js/p3-script.js
+++ assets/js/p3-script.js
@@ -3,27 +3,33 @@ var getSearchTerm = function() {
     var brewId = queryString.split("#")[1]
 generateBreweries(brewId)
 }
-
+var saveTasks = function() {
+                    localStorage.setItem("tasks", JSON.stringify(tasks));
+                  }
 var generateBreweries = function(brewId) {
     var apiUrl = "https://api.openbrewerydb.org/breweries/" + brewId;
     fetch(apiUrl)
     .then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
+
                 brewCall = document.querySelector(".brew-name")
                 brewCall.textContent = data.name;
+
                 var brewAddress = document.querySelector(".brew-address");
                 brewAddress.textContent = data.street + " " + data.city + ", " + data.state;
+
                 var brewPhone = document.querySelector(".phone");
                 brewPhone.textContent = data.phone;
+
                 var brewURL= document.createElement("a");
                 brewURL.setAttribute("href", data.website_url)
                 URLEl = document.querySelector(".url");
                 brewURL.textContent = data.website_url;
                 URLEl.appendChild(brewURL);
-                debugger;
                 }
             )}
     }
     )}
+    
     getSearchTerm();

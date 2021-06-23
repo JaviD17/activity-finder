4e757a4464474b9ce9decde43d07fc2daf03f106
Noctus <jonnyfaulkner21@gmail.com>
2021-06-22 23:06:23 -0500

    added map with dropped pin on the location of brewery

--- assets/js/p3-script.js
+++ assets/js/p3-script.js
@@ -1,8 +1,10 @@
+let brewMap = document.querySelector(".map")
+
 var getSearchTerm = function() {
     var queryString = document.location.href
     var brewId = queryString.split("#")[1]
 generateBreweries(brewId)
-}
+};
 
 var generateBreweries = function(brewId) {
     var apiUrl = "https://api.openbrewerydb.org/breweries/" + brewId;
@@ -16,9 +18,34 @@ var generateBreweries = function(brewId) {
                 brewAddress.textContent = data.street + " " + data.city + ", " + data.state;
                 var brewPhone = document.querySelector(".phone");
                 brewPhone.textContent = data.phone;
-                debugger;
+                getMap(data)
                 }
             )}
     }
-    )}
-    getSearchTerm();
+)};
+
+
+
+var getMap = function(data){
+    var mapScript = document.createElement("script");
+    mapScript.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBx_ugx7N5JSWJucIpYrHoPwoiEOrMOYNU&callback=initMap"
+    mapScript.async = true;
+
+    var latitude = data.latitude-0;
+    var longitude = data.longitude-0;
+    console.log(latitude, longitude)
+    window.initMap = function() {
+        map = new google.maps.Map(document.getElementById("map"), {
+          center: { lat: latitude, lng: longitude },
+          zoom: 10,
+        });
+        marker = new google.maps.Marker({
+            position: { lat: latitude, lng: longitude},
+            map: map
+        })
+    }
+
+    brewMap.appendChild(mapScript)
+}
+
+getSearchTerm();

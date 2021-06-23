deba0b007546a1f465b412e351d640b3a5eb44b5
E Lazar <emiliosalazar4@gmail.com>
2021-06-22 19:24:21 -0500

    added more fetch parameters

--- assets/js/p3-script.js
+++ assets/js/p3-script.js
@@ -1,21 +1,24 @@
 var getSearchTerm = function() {
     var queryString = document.location.href
-    var brewName = queryString.split("#")[1]
-    console.log(brewID)
-generateBreweries(brewName)
+    var brewId = queryString.split("#")[1]
+generateBreweries(brewId)
 }
 
-var generateBreweries = function() {
-    var apiUrl = "https://api.openbrewerydb.org/breweries?by_name" + brewName;
+var generateBreweries = function(brewId) {
+    var apiUrl = "https://api.openbrewerydb.org/breweries/" + brewId;
     fetch(apiUrl)
     .then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                
-                brewCall.textContent = brewName
+                brewCall = document.querySelector(".brew-name")
+                brewCall.textContent = data.name;
                 var brewAddress = document.querySelector(".brew-address");
                 brewAddress.textContent = data.street + " " + data.city + ", " + data.state;
+                var brewPhone = document.querySelector(".phone");
+                brewPhone.textContent = data.phone;
+                debugger;
                 }
             )}
     }
     )}
+    getSearchTerm();
